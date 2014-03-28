567fbbc3018474374a0f97d94b51a2bae37c1e96
billcreswell <billcreswell@gmail.com>
2014-03-27 21:30:39 -0400

    updates and de lang file

---
+++ lang/de.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @brief Search Language dictionary;
+ * @author: redcat http://www.pluck-cms.org/smf/index.php?action=profile;u=59722
+ * @fileinfo: data/modules/search/lang/de.php
+ */
+    $lang['search']['search'] = 'suchen';
+    $lang['search']['label'] = 'Bitte geben Sie das Suchwort ein.';
+    $lang['search']['info'] = 'Modul für Suchseite';
+    $lang['search']['results'] = 'Suchergebnis';
+	$lang['search']['no results'] = 'Leider kein Suchergebnis.';
+	$lang['search']['results for'] = 'Suchergebnisse für :';
+	$lang["search"]['enter a larger search term'] = 'Bitte geben Sie einen längeren Suchbegriff ein.';
+	$lang["search"]["cannot search symbols"] = "Eine Suche nach Symbolen ist nicht möglich.";
+?>
