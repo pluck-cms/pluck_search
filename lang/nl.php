9f9a60317e2a5aa207c13d7a265019d9e4e2a2c5
billcreswell <billcreswell@gmail.com>
2014-04-02 17:56:57 -0400

    nl translation by speedyb

---
+++ lang/nl.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @brief Search Language dictionary;
+ * @author: SpeedyB http://www.pluck-cms.org/smf/index.php?action=profile;u=60227
+ * @Website: http://www.xobit.nl
+ * @fileinfo: data/modules/search/lang/nl.php
+ */
+    $lang['search']['search'] = 'zoeken';
+    $lang['search']['label'] = 'Geef een zoekopdracht in.';
+    $lang['search']['info'] = 'Module voor zoeken';
+    $lang['search']['results'] = 'Zoekresultaten';
+	$lang['search']['no results'] = 'Helaas niets gevonden.';
+	$lang['search']['results for'] = 'Zoekresultaten voor :';
+	$lang["search"]['enter a larger search term'] = 'Geef een langere zoekopdracht in.';
+	$lang["search"]["cannot search symbols"] = "Helaas kan er niet gezocht worden op symbolen.";
+?>
